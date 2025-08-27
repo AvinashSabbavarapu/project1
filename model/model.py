26fc6ce832c5d0f3ecbf49f06a51193f24ce156a
AvinashSabbavarapu <105189044+AvinashSabbavarapu@users.noreply.github.com>
2025-08-27 16:40:53 +0530

    initial commit

--- model/model.py
+++ model/model.py
@@ -1,4 +1,3 @@
-# model/model.py
 import torch
 import torch.nn as nn
 import torchvision.models as models
@@ -18,20 +17,6 @@ class EncoderCNN(nn.Module):
 
         return self.linear(features)
 
-"""class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        super().__init__()
-        self.embed = nn.Embedding(vocab_size, embed_size)
-        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)  # ✅ FIXED
-        self.linear = nn.Linear(hidden_size, vocab_size)
-
-    def forward(self, features, captions):
-        embeddings = self.embed(captions[:, :-1])
-        inputs = torch.cat((features.unsqueeze(1), embeddings), 1)
-        hiddens, _ = self.lstm(inputs)
-        outputs = self.linear(hiddens)
-        return outputs """
-
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size):
         super(DecoderRNN, self).__init__()
@@ -45,3 +30,4 @@ class DecoderRNN(nn.Module):
         hiddens, _ = self.lstm(inputs)
         outputs = self.linear(hiddens)
         return outputs
+
